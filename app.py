88aa336239bd4bf4c096991fcea58fb2cbabe5df
Tomasz Wojakowski <49729749+Wojaqqq@users.noreply.github.com>
2021-12-15 16:48:06 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+apps = []
+
+if os.path.isfile('save.txt'):
+    with open('save.txt', 'r') as f:
+        tmp_apps = f.read()
+        tmp_apps = tmp_apps.split(",")
+        apps = [x for x in tmp_apps if x.strip()]
+
+
+def add_app():
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables", "*.exe"),
+                                                                                          ("all files", "*.*")))
+    apps.append(filename)
+
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+
+def run_apps():
+    for app in apps:
+        os.startfile(app)
+
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=add_app)
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42", command=run_apps)
+
+openFile.pack()
+runApps.pack()
+
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
+root.mainloop()
+
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
